8cc58e7b1f5d9deb05f534b1a48e3bc240e6d192
Kingdon <yebyen@gmail.com>
2017-10-22 00:19:00 -0400

    default public ip

--- main.tf
+++ main.tf
@@ -6,12 +6,17 @@ resource "aws_route53_record" "root" {
   name    = "acquatic.science"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_eip.lb.public_ip}"]
+  records = ["${var.public_ip}"]
 }
 resource "aws_route53_record" "star" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "acquatic.science"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_eip.lb.public_ip}"]
+  records = ["${var.public_ip}"]
+}
+variable "public_ip" {
+  type = "string"
+  description = "The IP to assign to the (whole) zone"
+  default = "54.81.43.221"
 }
