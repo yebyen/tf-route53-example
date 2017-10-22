5f2ca1795cd9a7750442c88367809f3e6a312654
Kingdon <yebyen@gmail.com>
2017-10-22 00:34:03 -0400

    star cname

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "aws_route53_record" "root" {
 }
 resource "aws_route53_record" "star" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
-  name    = "acquatic.science"
-  type    = "A"
+  name    = "*.acquatic.science"
+  type    = "CNAME"
   ttl     = "300"
-  records = ["${var.public_ip}"]
+  records = ["acquatic.science"]
 }
 variable "public_ip" {
   type = "string"
